4c384d27f02883ec651fddde9916d5ff79f921b3
Ahmed <ahmedetman610203@gmail.com>
2026-08-24 03:26:29 +0300

    edit 4

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware('auth')->group(function () {
     // ─── Products ───────────────────────────────────────────────────────────────
     Route::get('/products', [ProductController::class, 'index'])->name('products.index')->middleware('permission:products.view');
     Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware('permission:products.create');
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware('permission:products.view');
     Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('permission:products.update');
     Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('permission:products.delete');
 
